c2c20a2fe51279cbde3b0bfb1620826da06012a8
Narmadha J S <138014247+narmadhajs@users.noreply.github.com>
2024-08-04 16:35:23 +0530

    Create lab4.java

---
+++ lab4.java
@@ -0,0 +1,111 @@
+1.
+import java.util.StringTokenizer;
+class Simple{
+    public static void main(String[] args){
+        StringTokenizer st=new StringTokenizer("my name is khan"," ");
+        while(st.hasMoreTokens()){
+            System.out.println(st.nextToken());
+        }
+    }
+}
+
+2.
+import java.util.StringTokenizer;
+class Simple{
+    public static void main(String[] args){
+        StringTokenizer st=new StringTokenizer("my,name,is,khan");
+            System.out.println("Next token is:"+st.nextToken(","));
+    }
+}
+
+import java.util.StringTokenizer1;
+class StringTokenizer1{
+public static void main(String[] args){
+StringTokenizer st=new stringTokenizer("My friends are Keshika, Amogha, Tarunika, aayath, Aravin"," ");
+while(st.hasMoreTokens()){
+System.out.println(st.nextToken());
+}
+}
+}
+
+import java.util.StringTokenizer;
+class StringTokenizer1{
+    public static void main(String args[]){
+        StringTokenizer st=new StringTokenizer("My friends are Keshika, Amogha, Tarunika, aayath, Aravin"," ");
+        while(st.hasMoreElements()){
+            System.out.println(st.nextToken());
+        }
+    }
+}
+
+import java.util.StringTokenizer;
+class StringTokenizer3{
+    public static void main(String args[]){
+        StringTokenizer st=new StringTokenizer("My friends are Keshika, Amogha, Tarunika, aayath, Aravin"," ");
+        while(st.hasMoreTokens()){
+            System.out.println(st.nextElement());
+        }
+    }
+}
+import java.util.StringTokenizer;
+class StringTokenizer3{
+    public static void main(String args[]){
+        StringTokenizer st=new StringTokenizer("My friends are Keshika, Amogha, Tarunika, aayath, Aravin"," ");
+   
+            System.out.println("Total number of Tokens are:"+st.Tokens());
+        }
+    }
+}
+
+class Simple{
+    public static void main(String args[]){
+     arr= new int[5];
+     arr[0]=7;
+     arr[1]=11;
+     arr[2]=8;
+     arr[3]=9;
+     arr[4]=50;
+     for(int i=0;i<arr.length;i++)
+     System.out.println("Element at index"+i+":"+arr[i]);
+     
+    }
+}
+
+public class Simple{
+    public static void main(String args[]){
+    Simple[] arr;
+    arr=new Simple[5];
+    arr[0]=new Simple(1,"aman");
+     class Simple{
+     public int roll_no;
+     public String name;
+     Simple(int roll_no, String name){
+     this.roll_no=roll_no;
+     this.name=name;
+     }
+     
+    }
+}
+}
+
+
+public class Simple{
+    public static void main(String args[]){
+    Student[] arr;
+    arr=new Student[5];
+    arr[0]=new Student(1,"aman");
+    arr[1]=new Student(2,"Vibhav");
+    arr[2]=new Student(3,"Mohith");
+    arr[3]=new Student(3,"Vib");
+    for(int i=0;i<arr.length
+     class Student{
+     public int roll_no;
+     public String name;
+     Student(int roll_no, String name){
+     this.roll_no=roll_no;
+     this.name=name;
+     }
+     
+    }
+}
+}
